bc1b41ae8c9d121729ce42640641d934fff08552
auto-push <auto-push@example.com>
2023-09-22 16:07:43 +0000

    auto push from ci

--- widget/index.js
+++ widget/index.js
@@ -10,7 +10,7 @@ const makeIframeContent = (target) => {
     <link rel="stylesheet" href="${cssPath}">
     <base target="_parent" />
     <link>
-    <script nonce="5fe1fe2d653741262856a2dc6f4ce73225738659f54a0f48b9639c20fccd2ef5">
+    <script nonce="7bf79c751b3bfd454687788bdfecc6123863186258facc5cc18d72658a930360">
       window.CUSDIS_LOCALE = ${JSON.stringify(window.CUSDIS_LOCALE)}
       window.__DATA__ = ${JSON.stringify(target.dataset)}
     </script>
@@ -22,7 +22,7 @@ const makeIframeContent = (target) => {
   </head>
   <body>
     <div id="root"></div>
-    <script nonce="5fe1fe2d653741262856a2dc6f4ce73225738659f54a0f48b9639c20fccd2ef5" src="${iframeJsPath}" type="module">
+    <script nonce="7bf79c751b3bfd454687788bdfecc6123863186258facc5cc18d72658a930360" src="${iframeJsPath}" type="module">
 
     </script>
   </body>
